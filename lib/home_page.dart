e971847e858691d9ed2ff691fed94ec55747cad4
Manish <manishsinghsemra2@gmail.com>
2022-03-22 14:45:02 +0530

    day2 "data type widget"

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:matcher/matcher.dart';
+
+class HomePage extends StatelessWidget {
+  // const HomePage({Key? key}) : super(key: key);
+  int days = 30;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My First Mobile App"),
+      ),
+      body: Center(
+        child: Container(child: Text(
+            // "My name is Manish SIngh age $days $a $b ${bool} ${d} ${day} ${pi}"),
+            "My name is Manish SIngh age $days")),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  // const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("My name is Manish SIngh"),
-          ),
-        ),
-      ),
-    );
+    // double a = 20;
+    // String b = "Pass";
+    // bool c = true;
+    // num d = 20;
+    // var day = "Tuesday";
+    // const pi = 3.14;
+
+    return MaterialApp(home: HomePage());
   }
 }
